eceeb3784a22b821256bbe09cd26766e9939a1cb
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-03-09 00:13:31 -0300

    404 page fix

--- src/View/pageNotFound.php
+++ src/View/pageNotFound.php
@@ -1,12 +1,4 @@
 <?php require_once __DIR__.'/header.php'; ?>
-    <div class="alert alert-<?= $_SESSION['message_type']; ?> alert-dismissible fade show" role="alert">
-        <strong><?= (isset($_SESSION['strong_message'])) ? $_SESSION['strong_message'] : ''; ?></strong> <?= $_SESSION['message']; ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    <?php
-            unset($_SESSION['message_type']);
-            unset($_SESSION['message']);
-            unset($_SESSION['strong_message']);
-    ?>
+
 <?php require_once __DIR__.'/footer.php'; ?>   
 
