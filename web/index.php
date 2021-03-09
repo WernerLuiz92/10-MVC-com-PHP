7ddc162275af6daf78851dbcc6120be8b3c57e9a
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-03-08 23:01:42 -0300

    Testes

--- web/index.php
+++ web/index.php
@@ -9,25 +9,28 @@ session_start();
 require_once __DIR__.'/../vendor/autoload.php';
 
 $routes = require_once __DIR__.'/../config/routes.php';
-// $path = $_SERVER['PATH_INFO'];
+// PHP Localhost
+//$path = $_SERVER['PATH_INFO'];
 
-// var_dump($path);
+// Heroku
+$path = $_SERVER['REQUEST_URI'];
 
-// exit();
+var_dump($path);
 
-// if (!isset($_SERVER['PATH_INFO'])) {
-//     $path = '/';
-// } elseif (!array_key_exists($path, $routes)) {
-//     $path = '/*';
-// }
-$path = '/login';
+exit();
 
-// $isLoginRoute = stripos($path, 'login');
+if (!isset($_SERVER['PATH_INFO'])) {
+    $path = '/';
+} elseif (!array_key_exists($path, $routes)) {
+    $path = '/*';
+}
 
-// if (!isset($_SESSION['logged_user']) && $isLoginRoute === false && $path != '/' && $path != '/*') {
-//     header('Location: /login');
-//     exit();
-// }
+$isLoginRoute = stripos($path, 'login');
+
+if (!isset($_SESSION['logged_user']) && $isLoginRoute === false && $path != '/' && $path != '/*') {
+    header('Location: /login');
+    exit();
+}
 
 $psr17Factory = new Psr17Factory();
 
